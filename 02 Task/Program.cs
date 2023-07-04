cbbf8803eb67463d2efa45365a24e44551080892
expertz <xeniapliner@gmail.com>
2023-07-04 16:08:31 +0300

    Method 2 Task Done

--- 02 Task/Program.cs
+++ 02 Task/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты 
+//точки (X и Y), причем X ≠ 0 и Y ≠ 0 
+//и выдаёт номер четверти плоскости, 
+//в которой находится эта точка.
+
+
+int GetAreaNumber(int x, int y)
+{
+      int NumberArea = 0;
+
+      if (x > 0 && y > 0) NumberArea = 1;
+
+      if (x < 0 && y > 0) NumberArea = 2;
+
+      if (x < 0 && y < 0) NumberArea = 3;
+
+      if (x > 0 && y < 0) NumberArea = 4;
+
+      return NumberArea;
+}
+//------------------------------------------
+
+Console.Write("Put cordinate X:");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Put cordinate Y:");
+int y = int.Parse(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+      Console.WriteLine("Coordinates cannot be equal to 0");
+}      
+
+else 
+{
+      int result = GetAreaNumber(x, y);
+      Console.WriteLine($"Your pin is in {result} area");
+}
+
+
