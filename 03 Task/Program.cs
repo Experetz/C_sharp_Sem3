146bbf8195929f7c1d59e9c12e508033a5d0594d
expertz <xeniapliner@gmail.com>
2023-07-04 16:45:00 +0300

    Method 3 Task area code

---
+++ 03 Task/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой 
+//четверти (x и y).
+
+
+void ShowAreaRange(int k)
+{
+if (k < 1 || k > 4)
+{
+      Console.WriteLine("Area number could be only from 1 to 4");
+} 
+else 
+{
+      switch (k)
+      {
+            case 1:
+                  Console.WriteLine ("Range of possible coordinates is x > 0 and y > 0");
+                  break;
+            case 2:
+                  Console.WriteLine ("Range of possible coordinates is x < 0 and y > 0");
+                  break;
+            case 3:
+                  Console.WriteLine ("Range of possible coordinates is x < 0 and y < 0");
+                  break;
+            case 4:    
+                  Console.WriteLine ("Range of possible coordinates is x > 0 and y < 0");  
+                  break;
+      }
+}  
+}
+//------------------------------------------
+
+Console.Write("Put Area number ");
+int area = int.Parse(Console.ReadLine());
+ 
+ShowAreaRange(area);
+
+
