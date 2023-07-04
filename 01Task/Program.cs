8ac974815b5c1f4d564d215e0e8cc274bd935759
expertz <xeniapliner@gmail.com>
2023-07-04 15:41:51 +0300

    Method 2 Task

---
+++ 01Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// [Возвращаемый тип метода] [Имя метода - глагол ] (Параметры метода)
+//{
+//    Код метода
+//}
+
+int GetSum(int a, int b)
+{
+      int sum = a + b;
+      return sum;
+}
+
+void PrintMessageToConsole(string message)
+{
+   Console.WriteLine(message);   
+}
+
+//----------------------------
+PrintMessageToConsole("Programme start");
+int mySum = GetSum(10, 15);
+
+PrintMessageToConsole(mySum.ToString());
+
