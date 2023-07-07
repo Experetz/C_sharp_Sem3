7d9339b4e4d2e8a285824a826a37f9c63d3fb397
Expertz <xeniapliner@gmail.com>
2023-07-08 02:56:19 +0300

    HW all Tasks done

--- HW1 Task19/Program.cs
+++ HW1 Task19/Program.cs
@@ -2,22 +2,19 @@
 //число и проверяет, является ли оно палиндромом.\
 
 
-//Методы
+Console.WriteLine("Please put 5 digits number: ");
+int aByUser = int.Parse(Console.ReadLine());
+string polindrom = Convert.ToString(aByUser);
 
-int Prompt(string message)
+if (polindrom.Length != 5)
 {
-      Console.Write(message);
-      string value = Console.ReadLine();
-            if (value.Length != 5) 
-            {
-                  WriteLine("Error you need to put 5 digits number");
-            }
-            else
-            {
-      return result;
-            }
+    Console.WriteLine("You number is NOT 5 digits number");
 }
-
-
-string fivedigitsnumber = Prompt("Please put 5 digits number");
-
+else if (polindrom [0] == polindrom[4] && polindrom[1] == polindrom[3])
+{
+    Console.WriteLine($"You number: {polindrom} is the polindrom");
+}
+else
+{
+    Console.WriteLine("The number you put is not a polindrom");
+}
---
+++ HW3 Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+//Methods
+
+int ShowMessage(string message)
+{
+      Console.Write(message);
+      string value = Console.ReadLine();  
+      int result = Convert.ToInt32(value);
+      return result;
+}
+
+void PrintCubedNumber(int num)
+{ 
+int i = 1;      
+for (i = 1; i <= num; i++)
+Console.Write((Math.Pow(i, 3)) + "  ");
+}
+
+//Programme
+
+int userInput = ShowMessage("Please put a number ");
+if (userInput <= 0)
+{
+   Console.WriteLine("Invalid input. Please enter a positive number.");   
+}
+else 
+{
+PrintCubedNumber(userInput);
+}
